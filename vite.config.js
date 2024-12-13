b78f5105a5893a51e70c9af6549ecbe37e2dcb04
UZB02 <muhsinbekmirzamatov3@gmail.com>
2024-12-13 16:22:50 +0500

    xatolik

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
-      "@vueuse/motion": "@vueuse/motion",
+      "@vueuse/motion": require.resolve("@vueuse/motion"),
     },
   },
   build: {
