759ea9e3e82b8c2cd562ca2f06bc2df6f75a45cb
UZB02 <muhsinbekmirzamatov3@gmail.com>
2024-12-13 19:46:34 +0500

    vueuse install

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import './assets/main.css'
-// import { MotionPlugin } from "@vueuse/motion";
+import { MotionPlugin } from "@vueuse/motion";
+
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-// app.use(MotionPlugin);
 app.use(router)
+app.use(MotionPlugin);
 
 app.mount('#app')
