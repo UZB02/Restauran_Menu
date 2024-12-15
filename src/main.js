edfd8146d2ed6b4f67a4ebcf5e13e3e8e6ebbdeb
UZB02 <muhsinbekmirzamatov3@gmail.com>
2024-12-15 19:53:49 +0500

    vueuse animation added

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
 import './assets/main.css'
-// import { MotionPlugin } from "@vueuse/motion";
+import { MotionPlugin } from "@vueuse/motion";
+import { MotionDirective as motion } from "@vueuse/motion";
+
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-// app.use(MotionPlugin);
+app.use(MotionPlugin);
 app.use(router)
+
+export default {
+  directives: {
+    motion: motion(),
+  },
+};
 
 app.mount('#app')
